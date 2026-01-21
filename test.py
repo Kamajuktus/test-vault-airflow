c5524cb65d8ffc6b04bd54ed83abed3a96f7eccd
Kamajuktus <amirsaduov6@gmail.com>
2026-01-21 10:43:28 +0500

    datetime

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from airflow.utils.dates import days_ago
+from datetime import datetime, timedelta
 from airflow.operators.python import PythonOperator
 
 # The ID we stored in Vault: airflow/connections/postgres_analytics
@@ -10,7 +10,7 @@ def test_operator(conn_id):
 
 with DAG(
     'vault_demo_dag',
-    start_date=days_ago(1),
+    start_date=datetime(2026, 1, 23),
     schedule_interval=None,
     catchup=False
 ) as dag:
