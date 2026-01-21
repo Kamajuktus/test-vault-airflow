9442948d5545d811af92936693b1243db722361e
Kamajuktus <amirsaduov6@gmail.com>
2026-01-21 11:33:28 +0500

    fix

--- test.py
+++ test.py
@@ -14,7 +14,6 @@ def test_operator(conn_id):
 
 with DAG(
     'vault_demo_dag',
-    # FIXED: Set start_date to the past to ensure it can run immediately for testing
     start_date=datetime(2024, 1, 1), 
     schedule=None,
     catchup=False
@@ -23,9 +22,7 @@ with DAG(
     t1 = PythonOperator(
         task_id='query_db_securely',
         python_callable=test_operator,
-        # FIXED: op_kwargs must be a Dictionary {"arg_name": value}
         op_kwargs={"conn_id": DB_CONNECTION_ID} 
     )
 
-    # You don't need chain() or a list for a single task, 
-    # simply defining t1 inside the 'with DAG' block registers it.
+    t1
